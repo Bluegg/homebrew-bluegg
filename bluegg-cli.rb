c041635b275db05a7b55a6ecc6d832b8f68a585c
Kenny Heard <kennyheard@me.com>
2023-01-30 20:17:02 +0000

    Updated the Bluegg CLI tap to `v2.1.0`

--- bluegg-cli.rb
+++ bluegg-cli.rb
@@ -2,8 +2,8 @@
 class BlueggCli < Formula
     desc "The command-line companion for worthy developers at Bluegg. 🤖"
     homepage "https://github.com/Bluegg/bluegg-cli"
-    url "https://github.com/Bluegg/bluegg-cli/releases/download/v2.0.0/bluegg"
-    sha256 "2f5ba14387cb500f718ad97975ef70e460f5d4ced424ba65e97660046e90cbbd"
+    url "https://github.com/Bluegg/bluegg-cli/releases/download/v2.1.0/bluegg"
+    sha256 "4879eb4e4b27983a0aa392306e955a8e152f3b66fac74d8daff1cdf8560f41a0"
     license "MIT"
 
     def install
