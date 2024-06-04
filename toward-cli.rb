69be58edec0ca7c23f8c243b7b48d33e7bfc39b4
Charlie Dowley <dowleycharlotte@hotmail.co.uk>
2024-06-04 16:06:17 +0100

    v2.7.0

--- toward-cli.rb
+++ toward-cli.rb
@@ -2,8 +2,8 @@
 class TowardCli < Formula
     desc "The command-line companion for worthy developers at Toward. 🤖"
     homepage "https://github.com/towardstudio/toward-cli"
-    url "https://github.com/towardstudio/toward-cli/releases/download/v2.6.1/toward"
-    sha256 "fffae149b49538aa22de6465037a11771d027271bb37a8f289e9ff0417140fda"
+    url "https://github.com/towardstudio/toward-cli/releases/download/v2.7.0/toward"
+    sha256 "47f1eeaedbe7f9a75a1789d0abaa26d0f017606c7b5d5e66d0fb3425b654cf5a"
     license "MIT"
 
     def install
